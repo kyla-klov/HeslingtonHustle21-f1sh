12be3c732d004e5c2cc8162a1589b95aa5356541
peacock21777246 <cct529@york.ac.uk>
2024-05-09 13:33:13 +0100

    Small Change to GUITest

--- Game/tests/src/com/mygdx/game/tests/GUItest.java
+++ Game/tests/src/com/mygdx/game/tests/GUItest.java
@@ -3,12 +3,15 @@ package com.mygdx.game.tests;
 import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.Utils.Event;
 import com.badlogic.gdx.Gdx;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.mygdx.game.Objects.GUI;
@@ -16,7 +19,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(GdxTestRunner.class)
 public class GUItest {
+    private AutoCloseable closeable;
 
+    @Before
+    public void setup(){
+        closeable = MockitoAnnotations.openMocks(this);
+    }
     @Test
     public void testa(){
 
@@ -24,5 +32,9 @@ public class GUItest {
     @Test
     public void testb(){
 
+    }
+    @After
+    public void releaseMocks() throws Exception {
+        closeable.close();
     }
 }
