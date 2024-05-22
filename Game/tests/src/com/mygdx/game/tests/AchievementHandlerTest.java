77bcbb8b6b376989773618e28d1f3487f51be5c8
peacock21777246 <cct529@york.ac.uk>
2024-05-22 18:21:10 +0100

    Fix AchievementHandlerTest

--- Game/tests/src/com/mygdx/game/tests/AchievementHandlerTest.java
+++ Game/tests/src/com/mygdx/game/tests/AchievementHandlerTest.java
@@ -15,9 +15,9 @@ public class AchievementHandlerTest {
     Achievement bronzeBaller = new Achievement("Baller", "Score at least 8 points in basketball", Achievement.Type.BRONZE, "AchievementsDisplay/BallerAchievementBronze.png");
     Achievement silverBaller = new Achievement("Baller", "Score at least 10 points in basketball", Achievement.Type.SILVER, "AchievementsDisplay/BallerAchievementSilver.png");
     Achievement goldBaller = new Achievement("Baller", "Score at least 12 points in basketball", Achievement.Type.GOLD, "AchievementsDisplay/BallerAchievementGold.png");
-    Achievement bronzeDDG = new Achievement("Duck Duck Go", "Feed all of the ducks in under 20 seconds", Achievement.Type.BRONZE, "AchievementsDisplay/DuckAchievementBronze.png");
+    Achievement bronzeDDG = new Achievement("Duck Duck Go", "Feed all of the ducks in under 17 seconds", Achievement.Type.BRONZE, "AchievementsDisplay/DuckAchievementBronze.png");
     Achievement silverDDG = new Achievement("Duck Duck Go", "Feed all of the ducks in under 16 seconds", Achievement.Type.SILVER, "AchievementsDisplay/DuckAchievementSilver.png");
-    Achievement goldDDG = new Achievement("Duck Duck Go", "Feed all of the ducks in under 12 seconds", Achievement.Type.GOLD, "AchievementsDisplay/DuckAchievementGold.png");
+    Achievement goldDDG = new Achievement("Duck Duck Go", "Feed all of the ducks in under 15 seconds", Achievement.Type.GOLD, "AchievementsDisplay/DuckAchievementGold.png");
     Achievement bronzeFTF = new Achievement("Feast to Fullest", "Eat 3 times in a day", Achievement.Type.BRONZE, "AchievementsDisplay/AchievementPlaceHolder.png");
     Achievement silverFTF = new Achievement("Feast to Fullest", "Eat 4 times in a day", Achievement.Type.SILVER, "AchievementsDisplay/AchievementPlaceHolder.png");
     Achievement goldFTF = new Achievement("Feast to Fullest", "Eat 5 times in a day", Achievement.Type.GOLD, "AchievementsDisplay/AchievementPlaceHolder.png");
