2c9191fdcbb38f465a9ac1285b83d2614a942fac
peacock21777246 <cct529@york.ac.uk>
2024-05-22 12:03:29 +0100

    Slight Change to GameMusic

--- Game/core/src/com/mygdx/game/Objects/GameMusic.java
+++ Game/core/src/com/mygdx/game/Objects/GameMusic.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.Objects;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.utils.Disposable;
@@ -9,14 +10,24 @@ public class GameMusic implements Disposable {
     private float volume;
 
     public GameMusic() {
+        this("music_loop/Ludum Dare 30 - 01.ogg");
         // Load your music file
-        music = Gdx.audio.newMusic(Gdx.files.internal("music_loop/Ludum Dare 30 - 01.ogg"));
+
+//        music = Gdx.audio.newMusic(Gdx.files.internal("music_loop/Ludum Dare 30 - 01.ogg"));
+//        music.setLooping(true);  // If you want the music to loop
+//        volume = 0.5f;  // Default volume
+//        music.setVolume(volume);
+//        music.play();
+    }
+
+    public GameMusic(String filePath){
+        // Load your music file
+        music = Gdx.audio.newMusic(Gdx.files.internal(filePath));
         music.setLooping(true);  // If you want the music to loop
         volume = 0.5f;  // Default volume
         music.setVolume(volume);
         music.play();
     }
-
     public void setVolume(float volume) {
         this.volume = volume;
         music.setVolume(volume);
